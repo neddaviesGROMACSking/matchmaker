2b84c23a0a3cc5b55f9717d6e79372956240a15a
mark-todd <markpeter.todd@hotmail.co.uk>
2021-09-16 10:25:29 +0100

    Added advanced pytest packages
    Fixed version test

--- tests/test_matchmaker.py
+++ tests/test_matchmaker.py
@@ -1,5 +1,8 @@
+
 from matchmaker import __version__
+import pytest
 
-
-def test_version():
-    assert __version__ == '0.1.0'
+@pytest.mark.asyncio
+class TestBasic:
+    def test_version(self):
+        assert __version__ == '0.1.0'
