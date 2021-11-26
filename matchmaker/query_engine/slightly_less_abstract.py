3aa62e876e8f9aed76e93b1bb9f4a519f04aace3
mark-todd <markpeter.todd@hotmail.co.uk>
2021-11-26 15:37:38 +0000

    Reworked output of call on slightly less abstract to be an async iterator

--- matchmaker/query_engine/slightly_less_abstract.py
+++ matchmaker/query_engine/slightly_less_abstract.py
@@ -1,70 +1,49 @@
 from pydantic import BaseModel
-from typing import Callable, Generic, Type, TypeVar
+from typing import AsyncIterator, Callable, Generic, Type, TypeVar, List, Optional
 
 from matchmaker.query_engine.abstract import AbstractQueryEngine
 
-"""
 Metadata = TypeVar('Metadata')
-class AbstractNativeQuery(Generic[Metadata]):
-    metadata: Metadata
-    async def get_metadata(self):
-        raise NotImplementedError
-    def count_api_calls(self):
-        raise NotImplementedError('Calling method on abstract base class')
-    def count_api_calls_by_method(self, method: str):
-        raise NotImplementedError('Calling method on abstract base class')
-"""
 
-class AbstractNativeQuery:
+# Native data bound to async iterator?
+
+class AbstractNativeQuery(Generic[Metadata]):
     def count_api_calls(self):
         raise NotImplementedError('Calling method on abstract base class')
     def count_api_calls_by_method(self, method: str):
+        raise NotImplementedError('Calling method on abstract base class')
+    async def get_metadata(self) -> Metadata:
         raise NotImplementedError('Calling method on abstract base class')
 
 Query = TypeVar('Query')
-Data = TypeVar('Data')
+Data = TypeVar('Data', bound = AsyncIterator)
+DataElement = TypeVar('DataElement')
 NativeQuery = TypeVar('NativeQuery', bound=AbstractNativeQuery)
 NativeData = TypeVar('NativeData')
 
-"""
-from typing import List, Optional, Awaitable
-DataElement = TypeVar('DataElement')
-
-class DataGetter(Generic[DataElement, Metadata]):
-    _get_data: Awaitable[DataElement]
-    _get_metadata: Awaitable[Metadata]
-    _data: Optional[DataElement]
-    _metadata: Optional[Metadata]
-
-    def __init__(
-        self, 
-        get_data: Awaitable[DataElement], 
-        get_metadata: Awaitable[Metadata]
-    ) -> None:
-        self._get_data = get_data
-        self._get_metadata = get_metadata
-        self._data = None
-        self._metadata = None
-    
-    async def get_data(self) -> DataElement:
-        if self._data is None:
-            self._data = await self._get_data
-        return self._data
-    
+class DataOut(Generic[NativeQuery, Data, DataElement, Metadata], AsyncIterator):
+    _async_iter: Data
+    _get_metadata: Callable[[], Metadata]
+    _list: Optional[List[DataElement]]
+    def __init__(self, native_query: NativeQuery, data_async_iterator: Data) -> None:
+        self._async_iter = data_async_iterator
+        self._native_query = native_query
+        self._list = None
+    def __aiter__(self):
+        return self._async_iter.__aiter__()
+    async def __anext__(self):
+        return await self._async_iter.__anext__()
     async def get_metadata(self) -> Metadata:
-        if self._metadata is None:
-            self._metadata = await self._get_metadata
-        return self._metadata
+        return await self._native_query.get_metadata()
+    async def __getitem__(self, index: int):
+        if self._list is None:
+            self._list = [i async for i in self._async_iter]
+        return self._list[index]
 
-
-#data_getter = await get_paper_engine(query)
-#data = await data_getter.get_data()
-#metadata = await data_getter.get_metadata()
-
-
-
-
-class SlightlyLessAbstractQueryEngine(Generic[Query, NativeQuery, NativeData, Data], AbstractQueryEngine[Query, Data]):
+class SlightlyLessAbstractQueryEngine(
+    Generic[Query, NativeQuery, NativeData, Data, DataElement, Metadata], 
+    AbstractQueryEngine[Query, Data]
+):
     async def _query_to_native(self, query: Query) -> NativeQuery:
         raise NotImplementedError('Calling method on abstract base class')
     async def _run_native_query(self, query: NativeQuery) -> NativeData:
@@ -72,29 +51,12 @@ class SlightlyLessAbstractQueryEngine(Generic[Query, NativeQuery, NativeData, Da
     async def _post_process(self, query: Query, data: NativeData) \
             -> Data:
         raise NotImplementedError('Calling method on abstract base class')
-    async def __call__(self, query: Query) -> DataGetter:
-
+    async def __call__(self, query: Query) -> DataOut:
         nq = await self._query_to_native(query)
+        nd = await self._run_native_query(nq)
+        data_iter = await self._post_process(query, nd)
+        return DataOut[NativeQuery, Data, DataElement, Metadata](nq, data_iter)
 
-        async def get_metadata():
-            return nq.get_metadata()
-        
-        async def get_data():
-            return await self._post_process(query, await self._run_native_query(nq))
-        return DataGetter(get_data(), get_metadata())
-"""
-
-class SlightlyLessAbstractQueryEngine(Generic[Query, NativeQuery, NativeData, Data], AbstractQueryEngine[Query, Data]):
-    async def _query_to_native(self, query: Query) -> NativeQuery:
-        raise NotImplementedError('Calling method on abstract base class')
-    async def _run_native_query(self, query: NativeQuery) -> NativeData:
-        raise NotImplementedError('Calling method on abstract base class')
-    async def _post_process(self, query: Query, data: NativeData) \
-            -> Data:
-        raise NotImplementedError('Calling method on abstract base class')
-    async def __call__(self, query: Query) -> Data:
-        nd = await self._run_native_query(await self._query_to_native(query))
-        return await self._post_process(query, nd)
     async def get_native_query(self, query: Query) -> NativeQuery:
         return await self._query_to_native(query)
     async def get_data_from_native_query(self, query: Query, native_query: NativeQuery):
