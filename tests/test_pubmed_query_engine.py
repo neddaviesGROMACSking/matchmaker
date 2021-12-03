806a849d881b22a4a90ca0cbd1fbe46fca66e8bb
mark-todd <markpeter.todd@hotmail.co.uk>
2021-12-03 19:04:24 +0000

    Added new pubmed tests

--- tests/test_pubmed_query_engine.py
+++ tests/test_pubmed_query_engine.py
@@ -1,6 +1,7 @@
 import pytest
 from matchmaker.query_engine.backends.pubmed import PubmedBackend
-from matchmaker.query_engine.types.query import PaperSearchQuery, PaperIDHigh, EqualPredicate
+from matchmaker.query_engine.types.query import And, AuthorName, PaperSearchQuery, PaperIDHigh, EqualPredicate, Title
+from tests.conftest import pubmed_author_engine
 
 @pytest.mark.asyncio
 class TestPubmedBackend:
@@ -39,3 +40,49 @@ class TestPaperEngine:
         assert len(results_list) == 1
         result = results_list[0]
         assert result.paper_id.doi == '10.1242/dev.197293'
+    
+    async def test_pubmed_paper_engine_one_title(self, pubmed_paper_engine):
+        results = await pubmed_paper_engine(
+            PaperSearchQuery(
+                query = Title(
+                    operator = EqualPredicate(
+                        value = 'Fluorescently Labelled ATP Analogues for Direct Monitoring of Ubiquitin Activation.'
+                    )
+                ),
+                selector = {'title': True, 'source_title': True}
+            )
+        )
+        results = [i async for i in results]
+        assert len(results) == 1
+        result = results[0]
+        assert result == 1
+        assert result.title == 'Fluorescently Labelled ATP Analogues for Direct Monitoring of Ubiquitin Activation.'
+
+    async def test_pubmed_paper_engine_one_author_name(self, pubmed_paper_engine):
+        results = await pubmed_paper_engine(
+            PaperSearchQuery(
+                query = And(
+                    fields_ = [
+                        Title(
+                            operator = EqualPredicate(
+                                value = 'Fluorescently Labelled ATP Analogues for Direct Monitoring of Ubiquitin Activation'
+                            )
+                        ),
+                        AuthorName(
+                            operator = EqualPredicate(
+                                value = 'Stuber'
+                            )
+                        )
+                    ]
+                ),
+                selector = {
+                    'authors': {'preferred_name': {'surname': True}}
+                }
+            )
+        )
+        results = [i async for i in results]
+        assert len(results) == 1
+        result = results[0]
+        authors = result.authors
+        relevant_author_names = [author.preferred_name.surname for author in authors if author.preferred_name.surname == 'Stuber']
+        assert len(relevant_author_names) >= 1
